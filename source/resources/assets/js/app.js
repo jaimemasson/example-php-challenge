a89b5bdde216e8ffadd6ba79f60430233fd3224b
jaimemasson <jaimermasson@gmail.com>
2015-09-22 11:26:03 -0700

    setup basic laravel route and initial template.

---
+++ source/resources/assets/js/app.js
@@ -0,0 +1,3 @@
+var picmonic = angular.module('Picmonic', [
+	'ngRoute'
+]);
---
+++ source/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html ng-app="Picmonic">
+    <head>
+        <script src="/js/app.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="/css/app.min.css" />
+        <title>Picmonic Sample PHP Test</title>
+        <base href="/" />
+    </head>
+    <body>
+        <div ng-view class="container-fluid"></div>
+    </body>
+</html>
