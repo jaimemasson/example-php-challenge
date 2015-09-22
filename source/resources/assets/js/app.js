b22f8e1e7cb66746afa21ca11b14afc5e2961712
jaimemasson <jaimermasson@gmail.com>
2015-09-22 11:35:13 -0700

    add view and controller for index page

--- source/resources/assets/js/app.js
+++ source/resources/assets/js/app.js
@@ -1,3 +1,14 @@
 var picmonic = angular.module('Picmonic', [
 	'ngRoute'
 ]);
+picmonic.config(['$routeProvider', '$locationProvider',
+	function($routeProvider, $locationProvider){
+		$routeProvider.
+			when('/', {
+				templateUrl:'/partials/index.html',
+				controller: 'IndexController'
+			});
+		$locationProvider.html5Mode(true);
+	}
+]);
+picmonic.value("Commits", window.commits);
