08ac01181d899a774ef05e5787976d9713894db8
jaimemasson <jaimermasson@gmail.com>
2015-09-22 11:17:29 -0700

    set up gulp workflow

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,16 +1,27 @@
-var elixir = require('laravel-elixir');
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");
+var less = require("gulp-less");
+var minifyCss = require('gulp-minify-css');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+gulp.task("js", function() {
+  gulp.src(["bower_components/jquery/dist/jquery.js", "bower_components/bootstrap/dist/js/bootstrap.js", "bower_components/angular/angular.js", "bower_components/angular-route/angular-route.js","resources/assets/js/**/*.js"])            // Read the files
+    .pipe(concat("app.js"))   // Combine into 1 file
+    .pipe(uglify())                     // Minify
+    .pipe(rename({extname: ".min.js"})) // Rename to ng-quick-date.min.js
+    .pipe(gulp.dest("public/js"))            // Write minified to disk
+});
+gulp.task("css", function(){
+  gulp.src(["bower_components/bootstrap/dist/css/bootstrap.css", "resources/assets/less/**/*.less"])
+    .pipe(less())
+    .pipe(concat("app.css"))
+    .pipe(minifyCss())
+    .pipe(rename({extname: ".min.css"})) // Rename to ng-quick-date.min.js
+    .pipe(gulp.dest("public/css"))            // Write minified to disk
+});
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task("default", function() {
+  gulp.start("js");
+  gulp.start("css");
 });
