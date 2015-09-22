1f70ce9665b915352fa5757771a1a466c35e87e6
jaimemasson <jaimermasson@gmail.com>
2015-09-22 11:31:46 -0700

    add github pull and model save to home controller

--- source/app/Http/Controllers/HomeController.php
+++ source/app/Http/Controllers/HomeController.php
@@ -2,17 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
+
 use App\Http\Controllers\Controller;
+use App\Commit;
 
 class HomeController extends Controller
 {
-    
+   
     public function index()
     {
-        //
-         return view('index');
+        $commits = $this->updateAndReturnLatestCommits();
+        $commitsSorted = [];
+        foreach($commits as $commit){
+            $commitsSorted[$commit->name][] = $commit->toArray();
+        }
+        return view('index', ['commits' => json_encode($commitsSorted)]);
+    }
+
+    private function updateAndReturnLatestCommits(){
+
+        if($lastSavedCommit = Commit::orderBy('when', 'desc')->first())
+            $url = "https://api.github.com/repos/nodejs/node/commits?per_page=25&since=".gmDate("Y-m-d\TH:i:s\Z", strtotime($lastSavedCommit->created_at));
+        else
+            $url = "https://api.github.com/repos/nodejs/node/commits?per_page=25";
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_USERAGENT,'Picmonic PHP Test');
+        $data = curl_exec($curl);
+        $data = json_decode($data);
+        
+        curl_close($curl);
+
+        foreach($data as $commitData){
+            $commit = new Commit();
+            $commit->sha = $commitData->sha;
+            $commit->name = $commitData->commit->author->name;
+            $commit->message = $commitData->commit->message;
+            $commit->when = $commitData->commit->committer->date;
+            $commit->url = $commitData->commit->url;
+            $commit->save();
+        }
+        
+
+
+        return Commit::orderBy('when', 'desc')->take(25)->get();
     }
 
 }
--- source/resources/views/index.blade.php
+++ source/resources/views/index.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html ng-app="Picmonic">
     <head>
+        <script>
+            commits = {!! $commits !!};
+        </script>
         <script src="/js/app.min.js"></script>
         <link rel="stylesheet" type="text/css" href="/css/app.min.css" />
         <title>Picmonic Sample PHP Test</title>
